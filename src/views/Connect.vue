2ff7f00612c3bc2563cbd969b6430c1b9b6f0b14
loki_jarvis <loki.kodavati@gmail.com>
2023-05-07 17:51:37 -0400

    Initial Commit

---
+++ src/views/Connect.vue
@@ -0,0 +1,195 @@
+<template>
+  <div
+    class="
+      w-full
+      h-screen
+      absolute
+      text-center
+      flex
+      justify-center
+      items-center
+      bg-gray-100
+    "
+
+    style="background-image: url('path/to/image.jpg'); background-repeat: no-repeat; background-size: cover;"
+  >
+    <div class="flex flex-col">
+      <div class="py-10 flex flex-col text-center justify-center items-center">
+        <h1 class="text-6xl font-bold text-green-600">Connect your account</h1>
+        <p class="mt-5 text-sm max-w-lg text-blue-500">
+          We are using your metamask public key means your eth address as the id. sothat it secure the privacy no one can access you chat.
+        </p>
+      </div>
+      <div class="relative">
+        <div v-show="isLoading" class="absolute flex items-center justify-center w-full h-full z-10 bg-white-800">
+          <loader/>
+        </div>
+        <div class="grid grid-cols-4">
+          <button @click="isGen = true; getAccount()" :class="[isGen ? 'border-green-600 text-green-600 border-b-blue-700' : 'text-black border-blue-700 border-b-green-600']" class="hover:border-green-600 hover:text-green-600 border rounded-tl-lg col-span-4 px-10 py-2 ">Register</button>
+        </div>
+        <div v-show="isGen"  class="border grid grid-cols-3 border-green-600 border-t-transparentrounded-bl-lg rounded-br-lg">
+          <div class="col-span-3 pt-10 pb-2 flex items-center justify-center">
+            <div :style="{ backgroundImage: `url('${userForm.avatar}')` }" class="w-32 h-32 bg-center bg-contain bg-no-repeat">
+
+            </div>
+          </div>
+          <div class="text-black flex flex-col text-sm uppercase justify-start px-2 py-5">
+            <h1>Username</h1>
+            <input v-model="userForm.username" class="text-blue-500 text-center bg-transparent border-transparent outline-none" placeholder="type an username" type="text">
+          </div>
+          <div class="text-black flex flex-col text-sm uppercase justify-start px-2 py-5">
+            <h1>Avatar</h1>
+            <input v-model="userForm.avatar" class="text-blue-500 text-center bg-transparent border-transparent outline-none" placeholder="paste an picture" type="text">
+          </div>
+          <div @click="copy" class="text-black cursor-pointer flex flex-col text-sm uppercase justify-start px-2 py-5">
+            <h1>ID</h1>
+            <input disabled v-model="userForm.id" class="text-blue-500 text-center bg-transparent border-transparent outline-none" placeholder="type an username" type="text">
+          </div>
+        </div>
+        <div v-show="!isGen" class="border border-t-transparent grid grid-cols-3 border-green-600 border-dashed rounded-bl-lg rounded-br-lg">
+          <div class="col-span-3 pt-10 pb-2 flex items-center justify-center">
+            <div :style="{ filter: 'invert(100%)', backgroundImage: `url('http://www.team-energy.nl/wp-content/uploads/2019/12/HandschoenRechts-1.png')` }" class="w-32 h-32 bg-center bg-contain bg-no-repeat">
+
+            </div>
+          </div>
+          <div class="text-black flex flex-col text-sm col-start-2 uppercase justify-start px-2 py-5">
+            <h1>ID</h1>
+            <input disabled v-model="userForm.id" class="text-blue-500 text-center bg-transparent border-transparent outline-none" placeholder="type your id" type="text">
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-cols-3 mt-10 gap-10">
+        <button @click="$router.go(-1)" class="hover:border-green-600 hover:text-green-600 p-2 col-span-2 10 border border-blue-700 rounded-lg text-sm text-black">Take me back</button>
+        <button @click="next" class="border border-transparent hover:border-green-600 hover:bg-transparent p-2 10 bg-green-600 text-black rounded-lg">Continue</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapState, mapMutations } from "vuex";
+import {Multiaddr} from "multiaddr";
+import { setUser } from "../services/localstorage.service.js";
+import { createUser, validateCreation } from "../services/account.service.js";
+import Loader from '../components/Loader.vue'
+import { ipfsConfig as config, IPFS, Database, dbConfig } from "../services/storage.service.js";
+export default {
+  data() {
+    return {
+      isGen: true,
+      isLoading: false,
+      userForm: {
+        username: "",
+        avatar: "",
+        id: "",
+      },
+    };
+  },
+  computed: {
+    ...mapState(["ipfsNode", "user", "DBController"]),
+  },
+  methods: {
+    ...mapMutations(["setIpfsNode", "setDBController", "setMainDatabase", "setUser"]),
+    async connect(user) {
+      const node = await IPFS.create({
+        repo: 'devSend123',
+        EXPERIMENTAL: {pubsub: true},
+        config
+      });
+      this.setIpfsNode(node);
+      let id1 = await this.ipfsNode.id()
+      const id2=id1.id
+      console.log(id2.toString())
+      let validip4 = new Multiaddr('/ip4/172.31.207.209/tcp/4001/p2p/12D3KooWExixqRY39ULucKr7Wv7jjLNTpGwQkcThfw3MBKRk7ntv');
+      const resp = await node.bootstrap.add(validip4);
+      console.log(resp);
+      const controller = await Database.createInstance(this.ipfsNode,this.$options={id:id2.toString(),offline:false});
+      // const controller = await Database.createInstance(this.ipfsNode);
+      this.setDBController(controller);
+      const db = await controller.docs('desend-staging', dbConfig);
+      await db.load();
+      console.log(db)
+      const peers1 = await this.ipfsNode.swarm.peers()
+      console.log(peers1)
+      this.setMainDatabase(db);
+      if (this.isGen) { 
+        const chatsDb = await controller.docs(`${user.id}-chats`, dbConfig);
+        const users = await chatsDb.get('')
+        console.log(users)
+        await chatsDb.load();
+        const addr = await chatsDb.address.toString();
+        console.log("test")
+        console.log(addr)
+        return await db.put({
+          _id: user.id,
+          data: user,
+          chats: addr
+        }, {
+          pin: true
+        });
+      }
+      const found = await db.get(user.id);
+      console.log(test)
+      console.log(found)
+      if (found.length) return this.userForm = {
+        ...this.userForm,
+        username: found[0].data.username,
+        avatar: found[0].data.avatar
+      };
+      await this.DBController.stop();
+      this.setDBController(null)
+      this.setMainDatabase(null)
+      await this.ipfsNode.stop();
+      this.setIpfsNode(null)
+      throw new Error("User not found");
+    },
+    async getAccount() {
+      this.userForm = await createUser();
+    },
+    async copy() {
+      await navigator.clipboard.writeText(this.userForm.id);
+      this.$toasts.success('ID copied')
+    },
+    clear() {
+      for (var key in this.userForm) this.userForm[key] = ""
+    },
+    async next() {
+      this.isLoading = true;
+      const { status, errors } = await validateCreation(this.userForm)
+      if (!status && this.isGen) return errors.forEach(e => {
+        this.isLoading = false;
+        this.$toasts.error(e.message)
+      })
+      await this.copy()
+      try {
+        await this.connect(this.userForm);
+        this.$toasts.success('Successful connection.')
+        setUser(this.userForm)
+        this.setUser(this.userForm)
+        this.isLoading = false
+        this.$router.push('/app')
+      } catch (e) {
+        this.isLoading = false
+        console.log(e)
+        return this.$toasts.error(e.message)
+      }
+    }
+  },
+  async mounted() {
+    const user = this.user;
+    this.isLoading = true;
+    if (user) {
+      try {
+        await this.connect(user);
+      } catch (e) {
+        this.$toasts.error(e.message)
+      }
+      return this.$router.push("/app");
+    }
+    await this.getAccount();
+    this.isLoading = false;
+  },
+  components: {
+    Loader
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="w-full h-screen absolute text-center flex flex-col justify-center items-center bg-gray-100">
+    <div class="max-w-3xl mx-auto">
+      <h1 class="text-5xl font-bold text-green-600">Hive_Talk</h1>
+      <p class="mt-6 text-xl text-gray-500">
+        <span ref="typed" class="typed"></span>
+      </p>
+      <div class="mt-10">
+        <router-link tag="button" to="/connect" class="border border-green-600 bg-green-600 hover:bg-transparent text-white font-medium py-3 px-6 rounded-lg shadow-sm hover:shadow-md transition duration-300 ease-in-out">
+          Start
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Typed from "typed.js";
+
+export default {
+  mounted() {
+    const typed = new Typed(this.$refs.typed, {
+      strings: [
+        "Real-time Communication between the users over the network. Since this is a web application, users won’t need to install any software but instead login and start using the application."
+      ],
+      typeSpeed: 50,
+      backSpeed: 20,
+      backDelay: 1000,
+      loop: true,
+      showCursor: true,
+      cursorChar: "|",
+    });
+  },
+  beforeDestroy() {
+    // Destroy typed.js instance on component destroy
+    this.typed.destroy();
+  },
+};
+</script>
